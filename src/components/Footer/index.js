5ee59e5c4c11852de95519ce5e117d68957fd5d3
AnastasiyaBH <bhanastasia495@gmail.com>
2019-10-02 11:39:29 +0300

    feat: add Header nd Footer componenets

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const Footer = () => (
+    <footer className="footer">
+      <p className="footer__copyright">anastasiyaBH, 2019</p>
+    </footer>
+);
+
+export default Footer;
