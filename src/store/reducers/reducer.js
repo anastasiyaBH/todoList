84ab422e574d5bd9f880902a7638338e60052016
AnastasiyaBH <bhanastasia495@gmail.com>
2019-10-03 16:35:13 +0300

    feat: add reducer

---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,41 @@
+import { combineReducers } from 'redux';
+
+import * as actionTypes from '../actions/actionTypes';
+
+let ID = 1;
+
+const modalReducer = (state = false, action) => {
+    switch(action.type) {
+        case actionTypes.OPEN_MODAL: {
+            return true;
+        }
+        case actionTypes.CLOSE_MODAL: {
+            return false;
+        }
+        default: return state;
+    }
+}
+
+const listReducer = (state = [], action) => {
+  switch (action.type) {
+    case actionTypes.ADD_ITEM:{
+      return state.concat([
+        {
+          text: action.text, 
+          id: ++ID,
+        }
+      ]);
+    }
+    case actionTypes.REMOVE_ITEM: {
+      return state.filter((value) => value.id != action.id);
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export const reducer = combineReducers({
+  modalState: modalReducer,
+  listState: listReducer,
+});
